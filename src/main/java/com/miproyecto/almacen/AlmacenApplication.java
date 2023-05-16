1ab60c8993c1679d040b50a2caa5446881eadbd5
mgMarquez <mgmarquez@gmail.com>
2023-05-16 12:12:43 -0300

    properties

--- src/main/java/com/miproyecto/almacen/AlmacenApplication.java
+++ src/main/java/com/miproyecto/almacen/AlmacenApplication.java
@@ -4,12 +4,24 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AlmacenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlmacenApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 	@Bean
