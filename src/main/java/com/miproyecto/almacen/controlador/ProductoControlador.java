bda393be184c097438c5befadcf8e5b2e5ff314c
mgMarquez <mgmarquez@gmail.com>
2023-05-18 23:32:51 -0300

    API marcas funcionando

--- src/main/java/com/miproyecto/almacen/controlador/ProductoControlador.java
+++ src/main/java/com/miproyecto/almacen/controlador/ProductoControlador.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/producto")
+@RequestMapping("/api/productos")
 public class ProductoControlador {
 
     @Autowired
--- src/main/java/com/miproyecto/almacen/controlador/RubroControlador.java
+++ src/main/java/com/miproyecto/almacen/controlador/RubroControlador.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/rubro")
+@RequestMapping("api/rubros")
 public class RubroControlador {
     @Autowired
     private ImpRubroService service;
